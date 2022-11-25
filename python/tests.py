da5789211307d24bbeda870255257ee16e6666a2
Max Cunningham <maxzinedine@gmail.com>
2022-11-25 14:24:32 +0000

    Improved testing

--- python/tests.py
+++ python/tests.py
@@ -1,52 +1,64 @@
 import unittest
 
-from shopping_cart import ShoppingCartConcreteCreator
+from shopping_cart import ShoppingCartConcreteCreator, ShoppingCart
 from test_utils import Capturing, BasicFormatter, PriceFirstFormatter
 
 
+TOTAL = "total"
 class ShoppingCartTest(unittest.TestCase):
-    def test_print_receipt(self):
-        sc = ShoppingCartConcreteCreator().operation(BasicFormatter())
-        sc.add_item("apple", 2)
+    def perform(self,operations,formatter):
+        '''
+        operations : [
+            (key, amount, expected_outcome),
+            ..
+            ..
+            ..
+            (TOTAL,NA,expected_total)
+        ]
+        formatter : class Formatter (formatter.py)
+        '''
+        sc = ShoppingCartConcreteCreator().operation(formatter)
+        for (key, amount,_) in operations:
+            if key is not TOTAL:
+                sc.add_item(key, amount)
         with Capturing() as output:
             sc.print_receipt()
-        self.assertEqual("apple - 2 - 100", output[0])
-        self.assertEqual("Total - 200", output[1])
+        for i in range(len(operations)):
+            (_, _, expected_outcome) = operations[i]
+            self.assertEqual(expected_outcome, output[i])
+
+    def test_print_receipt(self):
+        operations = [
+            ("apple", 2, "apple - 2 - 100"),
+            (TOTAL,0,"Total - 200")
+        ]
+        self.perform(operations,BasicFormatter())
 
     def test_doesnt_explode_on_mystery_item(self):
-        sc = ShoppingCartConcreteCreator().operation(BasicFormatter())
-        sc.add_item("apple", 2)
-        sc.add_item("banana", 5)
-        sc.add_item("pear", 5)
-        with Capturing() as output:
-            sc.print_receipt()
-        self.assertEqual("apple - 2 - 100", output[0])
-        self.assertEqual("banana - 5 - 200", output[1])
-        self.assertEqual("pear - 5 - 0", output[2])
-        self.assertEqual("Total - 1200",output[3])
+        operations = [
+            ("apple", 2, "apple - 2 - 100"),
+            ("banana", 5, "banana - 5 - 200"),
+            ("pear", 5, "pear - 5 - 0"),
+            (TOTAL,0,"Total - 1200")
+        ]
+        self.perform(operations,BasicFormatter())
 
     def test_item_printed_in_order(self):
-        sc = ShoppingCartConcreteCreator().operation(BasicFormatter())
-        sc.add_item("banana", 5)
-        sc.add_item("apple", 2)
-        sc.add_item("pear", 5)
-        with Capturing() as output:
-            sc.print_receipt()
-        self.assertEqual("banana - 5 - 200",output[0])
-        self.assertEqual("apple - 2 - 100",  output[1])
-        self.assertEqual("pear - 5 - 0", output[2])
-        self.assertEqual("Total - 1200",output[3])
+        operations = [
+            ("banana", 5, "banana - 5 - 200"),
+            ("apple", 2, "apple - 2 - 100"),
+            ("pear", 5, "pear - 5 - 0"),
+            (TOTAL,0,"Total - 1200")
+        ]
+        self.perform(operations,BasicFormatter())
 
     def test_formatting_works(self):
-        sc = ShoppingCartConcreteCreator().operation(PriceFirstFormatter())
-        sc.add_item("banana", 5)
-        sc.add_item("apple", 2)
-        sc.add_item("pear", 5)
-        with Capturing() as output:
-            sc.print_receipt()
-        self.assertEqual("200 - 5 - banana",output[0])
-        self.assertEqual("100 - 2 - apple",  output[1])
-        self.assertEqual("0 - 5 - pear", output[2])
-        self.assertEqual("Total - 1200",output[3])
+        operations = [
+            ("banana", 5, "200 - 5 - banana"),
+            ("apple", 2, "100 - 2 - apple"),
+            ("pear", 5, "0 - 5 - pear"),
+            (TOTAL,0,"Total - 1200")
+        ]
+        self.perform(operations,PriceFirstFormatter())
 
 unittest.main(exit=False)
